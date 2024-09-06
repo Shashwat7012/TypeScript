0567a61f55881dd18b63909aad153c8bf4f75f6c
shashwat7012 <pandeyshashwatptu@gmail.com>
2024-09-06 04:21:37 -0300

    Adv. TypeScript

---
+++ 2_creating_alias.ts
@@ -0,0 +1,14 @@
+type Details = {name: string, marks: number, address? : string};
+// Details is kind of like an alias or a nickname to {name: string, marks: number, address? : string}
+
+const result1 : Details = {
+    name: "Shashwat",
+    marks: 98
+}
+
+
+const result2 : Details = {
+    name: "Rishabh",
+    marks: 95
+}
+
